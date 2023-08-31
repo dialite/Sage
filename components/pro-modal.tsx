e019834171fb31833072d8853491424b5b6d6bc8
dialite <raymondolisa01@gmail.com>
2023-08-31 21:45:17 +0100

    Finalize stripe payment processing

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import axios from "axios";
 import {
   Check,
   Code,
@@ -9,6 +10,7 @@ import {
   VideoIcon,
   Zap,
 } from "../node_modules/lucide-react";
+import { useState } from "react";
 
 import {
   Dialog,
@@ -59,6 +61,20 @@ const tools = [
 
 export const ProModal = () => {
   const proModal = useProModal();
+  const [loading, setLoading] = useState(false);
+
+  const onSubscribe = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get("/api/stripe");
+
+      window.location.href = response.data.url;
+    } catch (error) {
+      console.log(error, "STRIPE_CLIENT_ERROR");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
@@ -90,7 +106,12 @@ export const ProModal = () => {
           </DialogDescription>
         </DialogHeader>
         <DialogFooter>
-          <Button size="lg" variant="premium" className="w-full">
+          <Button
+            onClick={onSubscribe}
+            size="lg"
+            variant="premium"
+            className="w-full"
+          >
             Upgrade
             <Zap className="w-4 h-4 ml-2 fill-white" />
           </Button>
