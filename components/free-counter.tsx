5fba7e5d8eb032664702c55710ab89b03dd040a4
dialite <raymondolisa01@gmail.com>
2023-08-31 05:19:51 +0100

    Create api usage counter scale

--- components/free-counter.tsx
+++ components/free-counter.tsx
@@ -1,9 +1,12 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { Zap } from "../node_modules/lucide-react";
 
 import { Card, CardContent } from "@/components/ui/card";
-import {MAX_FREE_COUNTS} from "@/constants"
+import { MAX_FREE_COUNTS } from "@/constants";
+import { Progress } from "@/components/ui/progress";
+import { Button } from "@/components/ui/button";
 
 interface FreeCounterProps {
   apiLimitCount: number;
@@ -25,8 +28,17 @@ export const FreeCounter = ({ apiLimitCount = 0 }: FreeCounterProps) => {
       <Card className="bg-white/10 border-0">
         <CardContent className="py-6">
           <div className="text-center text-sm text-white mb-4 space-y-2">
-            <p>{apiLimitCount} / {MAX_FREE_COUNTS} Free Generations</p>
+            <p>
+              {apiLimitCount} / {MAX_FREE_COUNTS} Free Generations
+            </p>
+            <Progress
+              className="h-3"
+              value={(apiLimitCount / MAX_FREE_COUNTS) * 100}
+            />
           </div>
+          <Button className="w-full" variant="premium">
+            Upgrade <Zap className="w-4 h-4 ml-2 fill-white" />
+          </Button>
         </CardContent>
       </Card>
     </div>
